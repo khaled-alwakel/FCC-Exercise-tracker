87eae86cfa4bd9a118ee04ade3ad991c532253bc
Khaled Alwakel <khaledmofreh33@gmail.com>
2025-05-13 15:09:41 +0300

    no changes indeed

--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  "mongodb+srv://Khaled-Alwakel:Xor3KS9Hm1ffWxpT@cluster0.g1hid9r.mongodb.net/users?retryWrites=true&w=majority",
+  "mongodb+srv://Khaled-Alwakel:Xor3KS9Hm1ffWxpT@cluster0.g1hid9r.mongodb.net/users?retryWrites=true&w=majority"
 );
 
 const userSchema = new mongoose.Schema({
@@ -16,7 +16,8 @@ const userSchema = new mongoose.Schema({
       },
     ],
     default: [],
-  }
+    z,
+  },
 });
 
 module.exports = mongoose.model("User", userSchema);
